7b1bbc7ede461afdba8602b44c35c10bb41033dc
Haley Yii <haleyyii0705@gmail.com>
2025-04-14 15:47:14 -0700

    Initial commit

---
+++ devel/.private/fetchit_challenge/share/gennodejs/ros/fetchit_challenge/msg/_index.js
@@ -0,0 +1,34 @@
+
+"use strict";
+
+let SickCameraGoal = require('./SickCameraGoal.js');
+let SchunkMachineFeedback = require('./SchunkMachineFeedback.js');
+let SchunkMachineActionResult = require('./SchunkMachineActionResult.js');
+let SchunkMachineResult = require('./SchunkMachineResult.js');
+let SickCameraAction = require('./SickCameraAction.js');
+let SchunkMachineActionGoal = require('./SchunkMachineActionGoal.js');
+let SchunkMachineActionFeedback = require('./SchunkMachineActionFeedback.js');
+let SchunkMachineGoal = require('./SchunkMachineGoal.js');
+let SickCameraFeedback = require('./SickCameraFeedback.js');
+let SickCameraActionResult = require('./SickCameraActionResult.js');
+let SickCameraActionGoal = require('./SickCameraActionGoal.js');
+let SickCameraResult = require('./SickCameraResult.js');
+let SchunkMachineAction = require('./SchunkMachineAction.js');
+let SickCameraActionFeedback = require('./SickCameraActionFeedback.js');
+
+module.exports = {
+  SickCameraGoal: SickCameraGoal,
+  SchunkMachineFeedback: SchunkMachineFeedback,
+  SchunkMachineActionResult: SchunkMachineActionResult,
+  SchunkMachineResult: SchunkMachineResult,
+  SickCameraAction: SickCameraAction,
+  SchunkMachineActionGoal: SchunkMachineActionGoal,
+  SchunkMachineActionFeedback: SchunkMachineActionFeedback,
+  SchunkMachineGoal: SchunkMachineGoal,
+  SickCameraFeedback: SickCameraFeedback,
+  SickCameraActionResult: SickCameraActionResult,
+  SickCameraActionGoal: SickCameraActionGoal,
+  SickCameraResult: SickCameraResult,
+  SchunkMachineAction: SchunkMachineAction,
+  SickCameraActionFeedback: SickCameraActionFeedback,
+};
